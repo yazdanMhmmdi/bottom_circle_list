4c07190db533424491c29a180c6a87ea481667c0
Yazdan Mohammadi <yazdanmohammadi00@gmail.com>
2021-11-30 16:37:25 +0330

    add colors

---
+++ lib/constants/i_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class IColors {
+  static final Color backgroundColor = Color(0xffC4C4C4).withOpacity(0.5);
+  static final Color darkWhite = Color(0xffF5F5F5);
+  static final Color lightBlue = Color(0xffC6CFFF).withOpacity(0.65);
+  static final Color blue = Color(0xff2F89FC);
+  static final Color white = Color(0xffFFFFFF).withOpacity(0.85);
+  static final Color shadowColor = Color(0xff000000).withOpacity(0.25);
+  static final Color unselectedColor = Color(0xff000000).withOpacity(0.45);
+  static final Color selectedColor = Color(0xff000000).withOpacity(0.65);
+}
