4d43aa5d6ea5c696fc9ce027531227d7673c8971
Yazdan Mohammadi <yazdanmohammadi00@gmail.com>
2021-12-04 23:46:20 +0330

    add some colors

--- lib/constants/i_colors.dart
+++ lib/constants/i_colors.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
 class IColors {
-  static final Color backgroundColor = Color(0xffC4C4C4).withOpacity(0.5);
+  IColors._(); //prevent from initialize
+  static final Color backgroundColor = Color(0xffD4D4D4);
   static final Color darkWhite = Color(0xffF5F5F5);
-  static final Color lightBlue = Color(0xffC6CFFF).withOpacity(0.65);
-  static final Color blue = Color(0xff2F89FC);
-  static final Color white = Color(0xffFFFFFF).withOpacity(0.85);
+  static const Color lightBlue = Color(0xffDAE0FF);
+  static const Color blue = Color(0xff2F89FC);
+  static const Color white = Color(0xffFFFFFF);
   static final Color shadowColor = Color(0xff000000).withOpacity(0.25);
-  static final Color unselectedColor = Color(0xff000000).withOpacity(0.45);
-  static final Color selectedColor = Color(0xff000000).withOpacity(0.65);
+  static const Color unselectedIconColor = Color(0xff878787);
+  static const Color selectedIconColor = Color(0xff2D2F3A);
+  static const Color unselectedTextColor = Color(0xff878787);
+  static const Color selectedTextColor = Color(0xff2D2F3A);
+  static const Color splashColor = Color(0xffF0F0F0);
 }
